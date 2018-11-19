ad7b4ac92874310f5203c1286c58712e737ab3c6
ztss <1159875384@qq.com>
2018-11-19 18:59:05 +0800

    第六章练习

--- SixthPpractice.cpp
+++ SixthPpractice.cpp
@@ -55,3 +55,48 @@ int main()
 	cout<<a<<" "<<b<<endl;
 	return 0;
 }
+
+//p6.17
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+bool has_upper(const string &s);
+void to_lower(string &s);
+
+int main()
+{
+	string str="sTrssssss";
+	if(has_upper(str))
+	{
+		cout<<"str has upper character!"<<endl;
+	}
+	else
+	{
+		cout<<"yoho"<<endl;
+	}
+	to_lower(str);
+	cout<<str<<endl;
+	return 0;
+}
+
+bool has_upper(const string &s)
+{
+	for(auto i:s)
+	{
+		if(isupper(i))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void to_lower(string &s)
+{
+	for(auto &i:s)
+	{
+		i=tolower(i);
+	}
+}
