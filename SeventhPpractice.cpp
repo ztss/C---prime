d215fc1622165ff1bec729c4c61272d5af0e1c51
ztss <1159875384@qq.com>
2019-01-26 22:32:43 +0800

    第七章练习

--- SeventhPpractice.cpp
+++ SeventhPpractice.cpp
@@ -20,4 +20,43 @@ public:
 	}
 };
 
-//
+//p7.1.3
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+istream &read(istream &is,Person &item);
+ostream &print(ostream &os,const Person &item);
+
+class Person
+{
+private:
+    string name;
+	string address;
+public:
+    string GetName() const
+	{
+		return name;
+	}
+	string GetAddress() const
+	{
+		return address;
+	}
+};
+
+istream &read(istream &is,Person &item)
+{
+	is >> item.name >> item.address;
+	return is;
+}
+
+ostream &print(ostream &os,const Person &item)
+{
+	os << item.GetAddress() << " " << item.GetName() << endl;
+	return os;
+}
+
+//p7.10
+read函数返回的是istream的引用，所以read函数的返回值仍然可以作为read函数的第一个参数，
+这里是用来检验读入data1,data2数据的过程是否是正确的。
