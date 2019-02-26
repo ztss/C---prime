48a205fd3113d2d2f4ee13fc4b8cc4beef6b75ef
ztss <1159875384@qq.com>
2019-02-26 20:35:41 +0800

    第十章练习

--- tenthpractice.cpp
+++ tenthpractice.cpp
@@ -178,3 +178,168 @@ bool isShorter(const string & str1,const string &str2)
 {
 	return str1.size()<str2.size();
 }
+
+//p10.20
+#include<iostream>
+#include<list>
+#include<deque>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+void elimDups(vector<string> &sentance);
+bool isShorter(const string & str1,const string &str2);
+
+int main()
+{
+	vector<string> vs;
+	string str;
+	while(cin>>str)
+	{
+		vs.push_back(str);
+	}
+	for(auto vb=vs.cbegin();vb!=vs.cend();vb++)
+	{
+		cout << *vb << " ";
+	}
+	cout << endl;
+	elimDups(vs);
+	for(auto vb=vs.cbegin();vb!=vs.cend();vb++)
+	{
+		cout << *vb << " ";
+	}
+	cout << endl;
+	sort(vs.begin(),vs.end(),isShorter);
+	for(const auto &s: vs)
+	{
+		cout << s << " ";
+	}
+	cout << endl;
+	auto p_l=partition(vs.begin(),vs.end(),[](const string &str){return 3<str.size();});
+	for(auto vb=vs.begin();vb!=p_l;vb++)
+	{
+		cout << *vb << " ";
+	}
+	cout << endl;
+	auto num=count_if(vs.begin(),vs.end(),[](const string &str)->bool{
+	if(str.size()>6) return true; else return false;});
+	cout << num << endl;
+	return 0;
+}
+
+void elimDups(vector<string> &sentance)
+{
+	sort(sentance.begin(),sentance.end());
+	auto unique_s=unique(sentance.begin(),sentance.end());
+	sentance.erase(unique_s,sentance.end());
+}
+
+bool isShorter(const string & str1,const string &str2)
+{
+	return str1.size()<str2.size();
+}
+
+//p10.21
+#include<iostream>
+#include<list>
+#include<deque>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+void test_lambda()
+{
+	int a=5;
+	auto f=[a]()mutable->bool{if(a==0) return true; else a--; return false;};//这里也可以使用引用捕获达到同样的目的
+	for(int i=0;i<6;i++)
+	{
+		cout << f() << " " << endl;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	test_lambda();
+	return 0;
+}
+
+//p10.22
+#include<iostream>
+#include<list>
+#include<deque>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+#include<functional>
+using namespace std;
+using namespace std::placeholders;//注意这里
+
+bool greaterThen6(const string & str,int sz);
+
+int main()
+{
+	vector<string> vs;
+	string str;
+	while(cin>>str)
+	{
+		vs.push_back(str);
+	}
+	auto num=count_if(vs.cbegin(),vs.cend(),bind(greaterThen6,_1,6));
+	cout << num << endl;
+	return 0;
+}
+
+bool greaterThen6(const string & str,int sz)
+{
+	if(str.size()<=6)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+//p10.24
+#include<iostream>
+#include<list>
+#include<deque>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+#include<functional>
+using namespace std;
+using namespace std::placeholders;
+
+bool intGreaterstr(const int &a,const string &str)
+{
+	if(a>str.size())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	vector<int> vi={1,2,3,4,5,6,7,8,9,10};
+	string str="hello";
+	auto p=find_if(vi.cbegin(),vi.cend(),bind(intGreaterstr,_1,str));
+	cout << *p << endl;
+	return 0;
+}
