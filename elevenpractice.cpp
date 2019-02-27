f6cc34d08aed2309bb6824b4b26e17962f00e478
ztss <1159875384@qq.com>
2019-02-27 20:38:17 +0800

    第十一章练习

--- elevenpractice.cpp
+++ elevenpractice.cpp
@@ -92,3 +92,92 @@ int main()
 	cout << family.size() << endl;
 	return 0;
 }
+
+//p11.14
+#include<iostream>
+#include<list>
+#include<deque>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+#include<functional>
+#include<map>
+#include<set>
+using namespace std;
+using namespace std::placeholders;
+
+void add_family(map<string,vector<pair<string,string>>> &family,const string &str)
+{
+	if(family.find(str)==family.end())
+	{
+		family[str]=vector<pair<string,string>>();//建立一个空vector
+	}
+}
+
+void add_child(map<string,vector<pair<string,string>>> &family,const string &str1,const string &str2,const string &str3)
+{
+	family[str1].push_back({str2,str3});
+}
+
+int main()
+{
+	map<string,vector<pair<string,string>>> family;
+	add_family(family,"zhang");
+	add_child(family,"zhang","qiang","2011-1-1");
+	add_child(family,"liu","ming","1990-8-2");
+	cout << family.size() << endl;
+	return 0;
+}
+
+
+//p11.31
+#include<iostream>
+#include<list>
+#include<deque>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+#include<functional>
+#include<map>
+#include<set>
+using namespace std;
+using namespace std::placeholders;
+
+void remove_authors(multimap<string,string> &authors,const string &str)
+{
+	auto pos=authors.equal_range(str);
+	if(pos.first==pos.second)
+	{
+		cout << "delete faild!" << endl;
+	}
+	else
+	{
+		authors.erase(pos.first,pos.second);
+	}
+}
+
+void print_authors(multimap<string,string> &authors)
+{
+	for(auto &a:authors)
+	{
+		cout << a.first << " " << a.second << endl;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	multimap<string,string> authors;
+	authors.insert({"zhang","poi"});
+	authors.insert({"zhang","nip"});
+	authors.insert({"zhang","secret"});
+	authors.insert({"liu","liquid"});
+	print_authors(authors);
+	remove_authors(authors,"zhang");
+	print_authors(authors);
+	return 0;
+}
