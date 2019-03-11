7dfd1905b2402c7e75f23ea7c5441e8bf48a7be4
ztss <1159875384@qq.com>
2019-03-11 15:48:01 +0800

    第十四章练习

--- fourteenpractice.cpp
+++ fourteenpractice.cpp
@@ -3,3 +3,48 @@
 ++通常定义为类成员，因为他会改变对象的状态   ->必须定义为类成员，否则编译会报错
 <<通常定义为非成员 &&通常定义为非成员 ==通常定义为非成员
 ()通常定义为类成员，否则编译会报错。
+
+//p14.6 14.7 14.8
+friend ostream& operator<<(ostream &os,const Sales_data &sd);
+
+//p14.35 p14.36
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class ReadString{
+	public:
+		ReadString(istream &is=cin) : is(is){}
+		string operator()()
+		{
+			string line;
+			if(!getline(is,line))
+			{
+				line=" ";
+			}
+			return line;
+		}
+	private:
+	    istream& is;
+};
+
+int main()
+{
+	ReadString rs(cin);
+	vector<string> vs;
+	while(true)
+	{
+		string line=rs();
+		if(!line.empty())
+		{
+			vs.push_back(line);
+		}
+		else
+		{
+			break;
+		}
+	}
+	cout << vs.size() << endl;
+	return 0;
+}
