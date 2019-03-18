3758b7efe7da69959e3000bfd3c6e231a8da745d
ztss <1159875384@qq.com>
2019-03-18 19:25:50 +0800

    第十六章练习

--- sixteenpractice.cpp
+++ sixteenpractice.cpp
@@ -133,3 +133,119 @@ int main()
 	cout << get_size(a) << endl;
 	return 0;
 }
+
+
+//p16.12
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<memory>
+#include<list>
+#include<vector>
+using namespace std;
+
+template<typename T> class BlobPtr;
+template<typename T> class Blob;
+template<typename T>
+bool operator==(const Blob<T>&,const Blob<T>&);
+
+
+template<typename T> class Blob{
+	friend class BlobPtr<T>;
+	friend bool operator==<T>(const Blob<T>&,const Blob<T>&);
+	public:
+		typedef T value_type;
+		typedef typename vector<T>::size_type size_type;
+		Blob();
+		Blob(initializer_list<T> il);
+		size_type size() const
+		{
+			return data->size();
+		}
+		bool empty() const
+		{
+			return data->empty();
+		}
+		void push_back(const T &t)
+		{
+			data->push_back(t);
+		}
+		void pop_back();
+		//元素访问
+		T& back();
+		T& operator[](size_type i);
+	private:
+		shared_ptr<vector<T>> data;
+		void check(size_type i,const string &msg) const;
+};
+
+template<typename T>
+Blob<T>::Blob() : data(make_shared<vector<T>>()) {}
+
+template<typename T>
+Blob<T>::Blob(initializer_list<T> il) : data(make_shared<vector<T>>(il)) {}
+
+template<typename T>
+void Blob<T>::check(size_type i,const string& msg) const
+{
+	if(i>=data->size())
+	{
+		throw out_of_range(msg);
+	}
+}
+
+template<typename T>
+T& Blob<T>::back()
+{
+	check(0,"this is a empty Blob");
+	return data->back();
+}
+
+template<typename T>
+T& Blob<T>::operator[](size_type i)
+{
+	check(i,"subscipt out of range");
+	return (*data)[i];
+}
+
+template<typename T>
+void Blob<T>::pop_back()
+{
+	check(0,"this is a empty Blob");
+	data->pop_back();
+}
+
+template<typename T> class BlobPtr{
+	public:
+		BlobPtr() : curr(0) {}
+		BlobPtr(Blob<T> &a,size_t sz=0): wptr(a.data),curr(sz) {}
+		T& operator*() const
+		{
+			auto p=check(curr,"deference past end");
+			return (*p)[curr];
+		}
+		BlobPtr& operator++();
+		BlobPtr& operator--();
+	private:
+		shared_ptr<vector<T>> check(size_t,const string&) const;
+		weak_ptr<vector<T>> wptr;
+		size_t curr;
+};
+
+template<typename T>
+BlobPtr<T>& BlobPtr<T>::operator++()
+{
+
+}
+
+template<typename T>
+BlobPtr<T>& BlobPtr<T>::operator--()
+{
+
+}
+
+int main()
+{
+	Blob<int> squares={0,1,2,3,4,5,6,7,8,9};
+	return 0;
+}
