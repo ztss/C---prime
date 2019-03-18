68f8a7105cc96166c65e51387a94a9c72a47f974
ztss <1159875384@qq.com>
2019-03-18 19:25:26 +0800

    第十六章

--- sixteen.cpp
+++ sixteen.cpp
@@ -24,3 +24,34 @@
    }
    3.模板程序应该尽量减少对实参类型的要求。函数模板和类模板成员函数的定义通常放在头文件
    中。
+16.1.2 类模板
+   1.如下是一个类模板
+   template<typename T> class Blob{
+	 public:
+		typedef T value_type;
+		typedef typename vector<T>::size_type size_type;
+		Blob();
+		Blob(initializer_list<T> il);
+		size_type size() const
+		{
+			return data->size();
+		}
+		bool empty() const
+		{
+			return data->empty();
+		}
+		void push_back(const T &t)
+		{
+			data->push_back(t);
+		}
+		void pop_back();
+		//元素访问
+		T& back();
+		T& operator[](size_type i);
+	private:
+		shared_ptr<vector<T>> data;
+		void check(size_type i,const string &msg) const;
+   };
+   一个类模板的每一个实例都形成一个独立的类。每一个类与其他类型都没有任何关联。
+   2.默认情况下，对于一个实例化的类模板，其成员只有在使用时才被实例化。
+16.1.3 模板参数
